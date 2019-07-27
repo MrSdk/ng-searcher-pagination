5547d37f581ce57fe38c18867d511a80e7958041
mrsdk <mrsdk1902@gmail.com>
2019-07-28 02:53:55 +0500

    test

--- app.js
+++ app.js
@@ -8,9 +8,27 @@ var path = require("path")
 var app = express()
 
 var users = [
-    { name: "MrSdk", age: 21 },
-    { name: "Bilolbek", age: 28 },
-    { name: "Samandar", age: 14 },
+    { id: 1, name: "MrSdk", age: 21 },
+    { id: 2, name: "Bilolbek", age: 28 },
+    { id: 3, name: "Samandar", age: 14 },
+    { id: 4, name: "Sohib", age: 21 },
+    { id: 5, name: "Jalil", age: 28 },
+    { id: 6, name: "Eldor", age: 14 },
+    { id: 7, name: "Yusuf", age: 21 },
+    { id: 8, name: "Temur", age: 28 },
+    { id: 9, name: "Rashid", age: 14 },
+    { id: 10, name: "Mahmud", age: 21 },
+    { id: 11, name: "Jasur", age: 28 },
+    { id: 12, name: "Layliddin", age: 14 },
+    { id: 13, name: "Yoldosh", age: 21 },
+    { id: 14, name: "Bexruz", age: 28 },
+    { id: 15, name: "Bekzod", age: 14 },
+    { id: 16, name: "Saif", age: 21 },
+    { id: 17, name: "Said", age: 28 },
+    { id: 18, name: "Musurmon", age: 14 },
+    { id: 19, name: "Umid", age: 21 },
+    { id: 20, name: "Farrux", age: 28 },
+    { id: 21, name: "Jack", age: 14 },
 ]
 
 app.use(cors())
